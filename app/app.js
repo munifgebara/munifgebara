67b0693943cccf0fb6b0584d8ebf0b11b2224c56
Munif Gebara Junior <munifgebara@gmail.com>
2016-03-20 23:22:48 -0300

    linkando com youtube e github

--- app/app.js
+++ app/app.js
@@ -2,15 +2,17 @@ console.log('MunifApp started');
 var munifApp = angular.module("munifApp", []);
 
 munifApp.controller('reposController', ['$scope', '$http', function ($scope, $http) {
-
-    $http.get('https://api.github.com/users/munifgebara/repos')
+    $scope.repos = [];
+    $scope.msg = "";
+    //https://www.googleapis.com/youtube/v3/search?channelId=munifgebara&part=snippet,id&order=date&maxResults=2
+    $http.get('https://api.github.com/users/munifgebara/repos?sort=pushed&direction=desc')
         .success(
             function (result) {
-                console.log(result);
                 $scope.repos = result;
-
+                console.log($scope.repos);
             })
         .error(function (data, status) {
+            $scope.msg = "Problem loading repos";
 
         })
 }]);
