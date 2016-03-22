5119b101882ed46cba77b3bae993e3641f05ced8
Munif Gebara Junior <munifgebara@gmail.com>
2016-03-22 20:23:16 -0300

    filtro

--- app/app.js
+++ app/app.js
@@ -3,16 +3,15 @@ var munifApp = angular.module("munifApp", []);
 
 munifApp.controller('reposController', ['$scope', '$http', function ($scope, $http) {
     $scope.repos = [];
-    $scope.msg = "";
+    $scope.msg = "Carrengado Repositórios";
     //https://www.googleapis.com/youtube/v3/search?channelId=munifgebara&part=snippet,id&order=date&maxResults=2
     $http.get('https://api.github.com/users/munifgebara/repos?sort=pushed&direction=desc')
         .success(
             function (result) {
+                $scope.msg = result.length + " repositórios encontrados";
                 $scope.repos = result;
-                console.log($scope.repos);
             })
         .error(function (data, status) {
-            $scope.msg = "Problem loading repos";
-
+            $scope.msg = "Não foi possível carregar os repositórios";
         })
 }]);
